f886b572d9f9ff612a48ac91eeaac93c7994ac63
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-07-11 11:08:46 +0100

    Updates

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('AllCarsNI API Running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`AllCarsNI - Server started on port ${PORT}`)
+);

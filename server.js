431374d0c477c797df6d1250274ec26f2573e3a8
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-07-11 11:24:00 +0100

    Updates

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('AllCarsNI API Running'));
 
+// routes defined
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/cars', require('./routes/api/cars'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`AllCarsNI - Server started on port ${PORT}`)

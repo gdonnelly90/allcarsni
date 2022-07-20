ccb1212701b1e2e10bf552f5e6a53914ad896d4e
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-07-20 15:24:32 +0100

    Homepage Design Updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { Button } from 'react-bootstrap';
+import { MDBRow, MDBCol, MDBCollapse } from 'mdb-react-ui-kit';
 import homepage_main from '../assets/img/homepage_main.jpg';
 import home_hero from '../assets/img/home-hero.png';
 
@@ -9,6 +9,10 @@ const Home = () => {
     <>
       <MDBRow md='12'>
         <div className='home-row-1 text-center'>
+          <img
+            className='home-logo pb-5'
+            src={require('../assets/img/Mainlogo.png')}
+          />
           <img
             // src={homepage_main}
             src={home_hero}
@@ -24,23 +28,82 @@ const Home = () => {
       </MDBRow>
       <MDBRow md='12'>
         <div className='home-row-3 text-center'>
-          <h1>Row 3</h1>
+          <h1>Shop by car shape</h1>
         </div>
+        <MDBRow md='12'>
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-electric'
+              src={require('../assets/img/icons/electric.webp')}
+            />
+            <p>Electric</p>
+          </MDBCol>
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-suv'
+              src={require('../assets/img/icons/suv.webp')}
+            />
+            <p>SUV</p>
+          </MDBCol>{' '}
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-hatchback'
+              src={require('../assets/img/icons/hatchback.webp')}
+            />
+            <p>Hatchback</p>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow md='12'>
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-ccoupe'
+              src={require('../assets/img/icons/coupe.webp')}
+            />
+            <p>Coupe</p>
+          </MDBCol>
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-estate'
+              src={require('../assets/img/icons/estate.webp')}
+            />
+            <p>Estate</p>
+          </MDBCol>{' '}
+          <MDBCol md='4 text-center'>
+            <img
+              className='icon-convertible'
+              src={require('../assets/img/icons/convertible.webp')}
+            />
+            <p>Convertible</p>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol>
+            <div className='home-button gap-2 text-center pt-4 pb-5'>
+              <Button
+                className='home-button-browse'
+                variant='primary'
+                type='submit'
+              >
+                Browse all cars
+              </Button>
+            </div>
+          </MDBCol>
+        </MDBRow>
       </MDBRow>
       <MDBRow md='12'>
         <MDBCol md='4'>
           <div className='home-col-1 text-center'>
-            <h2>Col 1</h2>
+            <img src={require('../assets/img/cars/Bugatti_1.jpg')} />
           </div>
         </MDBCol>
         <MDBCol md='4'>
           <div className='home-col-2 text-center'>
-            <h2>Col 2</h2>
+            <img src={require('../assets/img/cars/Lamborghini_Urus_1.jpg')} />
           </div>
         </MDBCol>
         <MDBCol md='4'>
           <div className='home-col-3 text-center'>
-            <h2>Col 3</h2>
+            <img src={require('../assets/img/cars/Mercedes_S65_1.jpg')} />
           </div>
         </MDBCol>
       </MDBRow>

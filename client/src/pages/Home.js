825e2ed43fb8db2135aba227bde35172e568f0f3
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-07-20 13:38:05 +0100

    Home Page Design Testing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Car from '../components/Car';
-import axios from 'axios';
-// import cars from '../cars';
+import { Container } from 'react-bootstrap';
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import homepage_main from '../assets/img/homepage_main.jpg';
+import home_hero from '../assets/img/home-hero.png';
 
 const Home = () => {
-  const [cars, setCars] = useState([]);
-  useEffect(() => {
-    const fetchCars = async () => {
-      const { data } = await axios.get('/api/cars');
-
-      setCars(data);
-    };
-    fetchCars();
-  }, []);
-
   return (
     <>
-      <h1>Latest Cars</h1>
-      <Row>
-        {cars.map((car) => (
-          <Col key={car._id} sm={12} md={6} lg={4} xl={3}>
-            <Car car={car} />
-          </Col>
-        ))}
-      </Row>
+      <MDBRow md='12'>
+        <div className='home-row-1 text-center'>
+          <img
+            // src={homepage_main}
+            src={home_hero}
+            alt='Home Page Main Image'
+            class='responsive'
+          />
+        </div>
+      </MDBRow>
+      <MDBRow md='12'>
+        <div className='home-row-2 text-center'>
+          <h1>Row 2</h1>
+        </div>
+      </MDBRow>
+      <MDBRow md='12'>
+        <div className='home-row-3 text-center'>
+          <h1>Row 3</h1>
+        </div>
+      </MDBRow>
+      <MDBRow md='12'>
+        <MDBCol md='4'>
+          <div className='home-col-1 text-center'>
+            <h2>Col 1</h2>
+          </div>
+        </MDBCol>
+        <MDBCol md='4'>
+          <div className='home-col-2 text-center'>
+            <h2>Col 2</h2>
+          </div>
+        </MDBCol>
+        <MDBCol md='4'>
+          <div className='home-col-3 text-center'>
+            <h2>Col 3</h2>
+          </div>
+        </MDBCol>
+      </MDBRow>
     </>
   );
 };

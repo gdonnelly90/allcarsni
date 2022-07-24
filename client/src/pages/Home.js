7a11121fc9635688f6c5f6eac202fc6f39c86ba0
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-07-24 10:53:07 +0100

    Home page updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,128 +2,130 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import { AppShell } from '../AppShell';
+import SimpleSearch from '../components/search/SimpleSearch';
 import SimpleSearch2 from '../components/search/SimpleSearch2';
+import homepage_main from '../assets/img/homepage_main.jpg';
 import home_hero from '../assets/img/home-hero.png';
 import BlueBoxComponent from '../components/layout/BlueBoxComponent';
 
 const Home = () => {
   return (
-    <AppShell>
-      <main role='main' id='top'>
-        <main className='py3'>
-          <div className='main-gradient-colour'>
-            <MDBRow md='12'>
-              <div className='home-row-1 text-center'>
-                <img
-                  className='home-logo pb-5'
-                  src={require('../assets/img/logo.png')}
-                />
-                <img
-                  src={home_hero}
-                  alt='Home Page Main Image'
-                  className='responsive'
-                />
+    <main role='main' id='top'>
+      <main className='py3'>
+        <div className='main-gradient-colour'>
+          <MDBRow md='12'>
+            <div className='home-row-1 text-center'>
+              <img
+                className='home-logo pb-5'
+                src={require('../assets/img/logo.png')}
+              />
+              <img
+                // src={homepage_main}
+                src={home_hero}
+                alt='Home Page Main Image'
+                className='responsive'
+              />
+            </div>
+          </MDBRow>
+          <MDBRow md='12'>
+            <MDBCol>
+              <div className='home-row-2'>
+                {/* <SimpleSearch /> */}
+                <SimpleSearch2 />
               </div>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow md='12'>
+            <div className='home-row-3 text-center'>
+              <h1>Shop by car type</h1>
+            </div>
+            <MDBRow md='12'>
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-electric'
+                  src={require('../assets/img/icons/electric.webp')}
+                />
+                <p>Electric</p>
+              </MDBCol>
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-suv'
+                  src={require('../assets/img/icons/suv.webp')}
+                />
+                <p>SUV</p>
+              </MDBCol>{' '}
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-hatchback'
+                  src={require('../assets/img/icons/hatchback.webp')}
+                />
+                <p>Hatchback</p>
+              </MDBCol>
             </MDBRow>
             <MDBRow md='12'>
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-ccoupe'
+                  src={require('../assets/img/icons/coupe.webp')}
+                />
+                <p>Coupe</p>
+              </MDBCol>
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-estate'
+                  src={require('../assets/img/icons/estate.webp')}
+                />
+                <p>Estate</p>
+              </MDBCol>{' '}
+              <MDBCol md='4 text-center'>
+                <img
+                  className='icon-convertible'
+                  src={require('../assets/img/icons/convertible.webp')}
+                />
+                <p>Convertible</p>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
               <MDBCol>
-                <div className='home-row-2'>
-                  <SimpleSearch2 />
+                <div className='home-button gap-2 text-center pt-4 pb-5'>
+                  <Button
+                    className='home-button-browse'
+                    variant='primary'
+                    type='submit'
+                  >
+                    Browse all cars
+                  </Button>
                 </div>
               </MDBCol>
             </MDBRow>
-            <MDBRow md='12'>
-              <div className='home-row-3 text-center'>
-                <h1>Shop by car type</h1>
+          </MDBRow>
+          <MDBRow md='12'>
+            <div className='blie-box-row pb-4'>
+              <BlueBoxComponent />
+            </div>
+          </MDBRow>
+          <MDBRow md='12'>
+            <MDBCol md='4'>
+              <div className='home-col-1 text-center'>
+                <img src={require('../assets/img/cars/Bugatti_1.jpg')} />
               </div>
-              <MDBRow md='12'>
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-electric'
-                    src={require('../assets/img/icons/electric.webp')}
-                  />
-                  <p>Electric</p>
-                </MDBCol>
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-suv'
-                    src={require('../assets/img/icons/suv.webp')}
-                  />
-                  <p>SUV</p>
-                </MDBCol>{' '}
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-hatchback'
-                    src={require('../assets/img/icons/hatchback.webp')}
-                  />
-                  <p>Hatchback</p>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow md='12'>
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-ccoupe'
-                    src={require('../assets/img/icons/coupe.webp')}
-                  />
-                  <p>Coupe</p>
-                </MDBCol>
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-estate'
-                    src={require('../assets/img/icons/estate.webp')}
-                  />
-                  <p>Estate</p>
-                </MDBCol>{' '}
-                <MDBCol md='4 text-center'>
-                  <img
-                    className='icon-convertible'
-                    src={require('../assets/img/icons/convertible.webp')}
-                  />
-                  <p>Convertible</p>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow>
-                <MDBCol>
-                  <div className='home-button gap-2 text-center pt-4 pb-5'>
-                    <Button
-                      className='home-button-browse'
-                      variant='primary'
-                      type='submit'
-                    >
-                      Browse all cars
-                    </Button>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-            </MDBRow>
-            <MDBRow md='12'>
-              <div className='blie-box-row pb-4'>
-                <BlueBoxComponent />
+            </MDBCol>
+            <MDBCol md='4'>
+              <div className='home-col-2 text-center'>
+                <img
+                  src={require('../assets/img/cars/Lamborghini_Urus_1.jpg')}
+                />
               </div>
-            </MDBRow>
-            <MDBRow md='12'>
-              <MDBCol md='4'>
-                <div className='home-col-1 text-center'>
-                  <img src={require('../assets/img/cars/Bugatti_1.jpg')} />
-                </div>
-              </MDBCol>
-              <MDBCol md='4'>
-                <div className='home-col-2 text-center'>
-                  <img
-                    src={require('../assets/img/cars/Lamborghini_Urus_1.jpg')}
-                  />
-                </div>
-              </MDBCol>
-              <MDBCol md='4'>
-                <div className='home-col-3 text-center'>
-                  <img src={require('../assets/img/cars/Mercedes_S65_1.jpg')} />
-                </div>
-              </MDBCol>
-            </MDBRow>
-          </div>
-        </main>
+            </MDBCol>
+            <MDBCol md='4'>
+              <div className='home-col-3 text-center'>
+                <img src={require('../assets/img/cars/Mercedes_S65_1.jpg')} />
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </div>
       </main>
-    </AppShell>
+    </main>
   );
 };
 
--- client/src/pages/Home2.js
+++ client/src/pages/Home2.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import home_image from '../assets/img/fullwidth/bugatti_back3.jpg';
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import home_logo from '../assets/img/logo.png';
+import home_car from '../assets/img/fullwidth/bugatti_back3.jpg';
 
 const Home2 = () => {
   return (
-    <main role='main' id='top'>
-      <div className='py3'>
-        <div className='main-gradient-colour'>
-          <Row md='12'>
-            <div className='home-row-1 text-center'>
-              <img
-                className='home-logo pb-5'
-                src={require('../assets/img/logo.png')}
-              />
-              <img
-                src={home_image}
-                alt='Home Page Main Image'
-                className='responsive'
-              />
-            </div>
-          </Row>
-        </div>
-      </div>
-    </main>
+    <div className='home-row-1 text-center'>
+      <img className='home-logo pb-5' src={home_logo} />
+      <img
+        src={home_car}
+        alt='Home Page Main Image'
+        // className='responsive'
+      />
+    </div>
   );
 };
 
 export default Home2;
 
 {
-  /* // <section className='home-image'>
-              // <>
-              //   <a className='home-row-1 text-center'>
-              //     <img
-              //       className='home-logo pb-5'
-              //       src={require('../assets/img/logo.png')}
-              //     />
-              //   </a>
-              //   <a className='text-center'>
-              //     <img src={require('../assets/img/fullwidth/bugatti_back3.jpg')} />
-              //   </a>
-              // </>
-// src={require('../assets/img/logo.png')} */
+  /* <div className='home-row-1'>
+  <img className='home-logo' src={require('../assets/img/logo.png')} />
+  <img src={home_car} alt='Home Page Main Image' className='responsive' />
+</div> */
 }
+
+<main role='main' id='top'>
+  <div className='home-logo-image'>
+    <img className='home-logo' src={home_logo} />
+  </div>
+
+  <div className='home-car'>
+    <img className='home-car-img' src={home_car} />
+  </div>
+</main>;

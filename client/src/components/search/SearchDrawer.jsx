e045873c78e3edf4fdc1979d2c301c3a26bb458f
Gary Donnelly <gdonnelly90@qub.ac.uk>
2022-08-21 21:44:54 +0100

    Filter Updates

--- client/src/components/search/SearchDrawer.jsx
+++ client/src/components/search/SearchDrawer.jsx
@@ -9,9 +9,10 @@ import { MODEL } from '../../utils/constants';
 import { FUEL_TYPE } from '../../utils/constants';
 import { BODYTYPE } from '../../utils/constants';
 import { Checkbox } from './Checkbox';
+import { Box } from '@mui/material';
 
 export const SearchDrawer = ({ makes, models, colours, onSearchValueChange }) => {
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
   const [activeStatus, setActiveStatus] = useState('');
   const [activeMake, setActiveMake] = useState('');
   const [activeModel, setActiveModel] = useState('');
@@ -97,46 +98,24 @@ export const SearchDrawer = ({ makes, models, colours, onSearchValueChange }) =>
     onSearchValueChange({ key: 'fuelType', value });
   };
 
-  // const handleCheck = (e) => {
-  //   // const {target: { value },
-  //   // } = e;
-  //   if (e.{target: { value }}.checked) {
-  //     setActiveStatus(value, activeStatus);
-  //     onSearchValueChange({ key: 'isNewCar', value: 'true' });
-  //   }
-  // };
+  const resetFilters = () => {
+    // window.history.replaceState(null, '', '/search?page=1');
+    // window.location.reload('/search?page=1');
+    window.location.href = '/search?page=1';
+  };
 
   return (
     <div className='search-drawer-container p-5'>
-      <p className='siderbar-text'>Filters</p>
+      <div className='siderbar-text'>Filters</div>
+
+      <Box className='reset-filters-button mb-5'>
+        <Button onClick={resetFilters}>Reset</Button>
+      </Box>
+
       <div className='dropdown'>
         <p className='siderbar-filter-text mt-3'>Car Status</p>
         <Checkbox label=' Select for New Cars only' onChange={(e) => onStatusChange(e)} />
-        {/* <p className='siderbar-filter-text mt-2'>Select for New Cars Only</p> */}
-        {/* <div class='form-check mb20'>
-          <input class='form-check-input' type='checkbox' value='' id='flexCheckPetrolSbp' />
-          <label class='form-check-label' for='flexCheckPetrolSbp'>
-            Select for New Cars Only
-          </label>
-        </div> */}
-        {/* input value='isNewCar' type='checkbox' onChange={handleCheck} /> */}
-        {/* Vegetarisma Farm <input type='checkbox' checked /> <span className='check'></span>{' '} */}
-        {/* <div>
-          <Form.Select
-            className='filter-holder-text'
-            aria-label='select dropdown'
-            id='status'
-            name='status'
-            onChange={(e) => onStatusChange(e)}>
-            <option>Select New or Used Condition</option>
-            <option selected={activeStatus === STATUS.NEW} value={STATUS.TRUE}>
-              {STATUS.NEW}
-            </option>
-            <option selected={activeStatus === STATUS.USED} value={STATUS.FALSE}>
-              {STATUS.USED}
-            </option>
-          </Form.Select>
-        </div> */}
+
         <p className='siderbar-filter-text mt-5'>Make & Model</p>
         <div>
           <Form.Select
@@ -157,6 +136,7 @@ export const SearchDrawer = ({ makes, models, colours, onSearchValueChange }) =>
           </Form.Select>
         </div>
       </div>
+
       <Form.Select
         className='filter-holder-text mt-3'
         aria-label='select dropdown'
@@ -315,78 +295,37 @@ export const SearchDrawer = ({ makes, models, colours, onSearchValueChange }) =>
 };
 
 {
-  /* <p className='siderbar-filter-text mt-5'>Engine Specs</p>
-<button
-  className='drawer-button dropdown-toggle rounded'
-  type='button'
-  data-bs-toggle='dropdown'
-  aria-expanded='false'>
-  Max Mileage
-</button>
-<ul className='dropdown-menu'>
-  <li>
-    <a className='dropdown-item' onClick={() => onMileageChange(PRICE_MILEAGE_FIGURE.FIVE)}>
-      20000
-    </a>
-  </li>
-  <li>
-    <a className='dropdown-item' onClick={() => onMileageChange(PRICE_MILEAGE_FIGURE.ELEVEN)}>
-      50000
-    </a>
-  </li>
-  <li>
-    <a
-      className='dropdown-item'
-      onClick={() => onMileageChange(PRICE_MILEAGE_FIGURE.SEVENTEEN)}>
-      80000
-    </a>
-  </li>
-</ul> */
+  /* <p className='siderbar-filter-text mt-2'>Select for New Cars Only</p> */
 }
-
-// <div className='dropdown'>
-//   <button
-//     className='drawer-button dropdown-toggle rounded mt-4'
-//     type='button'
-//     data-bs-toggle='dropdown'
-//     aria-expanded='false'>
-//     Fuel Type
-//   </button>
-//   <ul className='dropdown-menu'>
-//     <li>
-//       <a className='dropdown-item' onClick={() => onFuelChange(FUEL_TYPE.ELECTRIC)}>
-//         Electric
-//       </a>
-//     </li>
-//     <li>
-//       <a className='dropdown-item' onClick={() => onFuelChange(FUEL_TYPE.PETROL)}>
-//         Petrol
-//       </a>
-//     </li>
-//     <li>
-//       <a className='dropdown-item' onClick={() => onFuelChange(FUEL_TYPE.DIESEL)}>
-//         Diesel
-//       </a>
-//     </li>
-//   </ul>
-// </div>
-
-// const onColourClick = (colour) => {
-//   navigate(`?colour=${colour}`);
-// };
-
-// const onMinPriceClick = (price) => {
-//   navigate(`?price[gte]=${price}`);
-// };
-
-// const onMaxPriceClick = (price) => {
-//   navigate(`?price[lte]=${price}`);
-// };
-
-// const onMaxMileageClick = (mileage) => {
-//   navigate(`?mileage[lte]=${mileage}`);
-// };
-
-// const onFuelTypeClick = (fuelType) => {
-//   navigate(`?fuelType=${fuelType}`);
-// };
+{
+  /* <div class='form-check mb20'>
+          <input class='form-check-input' type='checkbox' value='' id='flexCheckPetrolSbp' />
+          <label class='form-check-label' for='flexCheckPetrolSbp'>
+            Select for New Cars Only
+          </label>
+        </div> */
+}
+{
+  /* input value='isNewCar' type='checkbox' onChange={handleCheck} /> */
+}
+{
+  /* Vegetarisma Farm <input type='checkbox' checked /> <span className='check'></span>{' '} */
+}
+{
+  /* <div>
+          <Form.Select
+            className='filter-holder-text'
+            aria-label='select dropdown'
+            id='status'
+            name='status'
+            onChange={(e) => onStatusChange(e)}>
+            <option>Select New or Used Condition</option>
+            <option selected={activeStatus === STATUS.NEW} value={STATUS.TRUE}>
+              {STATUS.NEW}
+            </option>
+            <option selected={activeStatus === STATUS.USED} value={STATUS.FALSE}>
+              {STATUS.USED}
+            </option>
+          </Form.Select>
+        </div> */
+}
